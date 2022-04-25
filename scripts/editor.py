f80319ea8e29be9e1642c15d9725eaa47feb37e1
mynameispet <ultraswordkirbypink@gmail.com>
2022-04-25 14:30:15 -0400

    eadw

---
+++ scripts/editor.py
@@ -0,0 +1,21 @@
+"""
+editor.py
+
+- contains Editor object
+- allows for tilemap editing
+- extends off the world.World object from engine
+
+"""
+
+from engine import world
+
+from scripts import art, WindowObject
+from scripts.globals import *
+
+
+class Editor(WindowObject, world.World):
+    """"""
+    pass
+
+
+
